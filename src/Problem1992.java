408decf174ab8321753c6281f2e82e4cd17bb937
KIYOUNG KWON <kkywalk2@gmail.com>
2021-08-12 23:04:26 +0900

    문제풀이 1992번

---
+++ src/Problem1992.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Problem1992 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[][] rectangle = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            String[] token = sc.next().split("");
+            for (int j = 0; j < length; j++)
+                rectangle[i][j] = Integer.parseInt(token[j]);
+        }
+        sc.close();
+        divAndConq(0, 0, length, rectangle);
+    }
+
+    public static void divAndConq(int x, int y, int n, int[][] rectangle) {
+        int v = rectangle[x][y];
+        if (caculate(v, x, y, n, rectangle)) {
+            switch (v) {
+                case 0:
+                    System.out.print("0");
+                    return;
+                case 1:
+                    System.out.print("1");
+                    return;
+                default:
+                    throw new RuntimeException("Invalid Input!");
+            }
+        }
+
+        int div = n / 2;
+        if (div == 0)
+            return;
+
+        System.out.print("(");
+        divAndConq(x, y, div, rectangle);
+        divAndConq(x, y + div, div, rectangle);
+        divAndConq(x + div, y, div, rectangle);
+        divAndConq(x + div, y + div, div, rectangle);
+        System.out.print(")");
+    }
+
+    public static Boolean caculate(int v, int x, int y, int n, int[][] rectangle) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (v != rectangle[x + i][y + j])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
