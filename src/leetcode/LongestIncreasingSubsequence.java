8757dd0daa1d855b1acf5d4427c33130bffa3b12
KIYOUNG KWON <kkywalk2@gmail.com>
2021-12-24 00:39:49 +0900

    leetcode dynamic-programming medium 풀이완료

--- src/leetcode/LongestIncreasingSubsequence.java
+++ src/leetcode/LongestIncreasingSubsequence.java
@@ -1,5 +1,41 @@
 package leetcode;
 
 public class LongestIncreasingSubsequence {
-    
+    public int lengthOfLIS(int[] nums) {
+        int[] lis = new int[nums.length];
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (size == 0 || lis[size - 1] < nums[i]) {
+                lis[size++] = nums[i];
+            } else {
+                binarySearch(lis, nums[i], size);
+            }
+        }
+        return size;
+    }
+
+    public void binarySearch(int[] lis, int num, int size) {
+        int left = 0;
+        int right = size;
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (lis[mid] == num) {
+                return;
+            }
+
+            if (lis[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        lis[right] = num;
+    }
+
+    public static void main(String[] args) throws Exception {
+        LongestIncreasingSubsequence longestIncreasingSubsequence = new LongestIncreasingSubsequence();
+        longestIncreasingSubsequence.lengthOfLIS(new int[]{3,5,6,2,5,4,19,5,6,7,12});
+        //longestIncreasingSubsequence.lengthOfLIS(new int[] {3,2,5,2,3,1,4});
+    }
 }
